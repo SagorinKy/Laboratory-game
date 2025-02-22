6bfc27a43a971bd29486c2c3eaac2efd8d36ceb5
Kirill Sagorin <st128530@student.spbu.ru>
2025-02-23 00:43:44 +0300

    simples files for tests

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "test.h"
+
+int main()
+{
+	std::cout<<"hello main \n";
+
+	return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include "test.h"
+
+bool test(int a)
+{
+	if (a == 1)
+		return true;
+	return false;
+}
+
